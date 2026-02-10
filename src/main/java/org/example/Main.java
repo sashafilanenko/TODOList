0737f072402679c6a6282f58c46ba20cd0dfc045
sashafilanenko <sashafilanenko@gmail.com>
2026-02-11 01:14:49 +0300

    WIP: Implement main class and apply minor updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
+import org.example.controller.TaskController;
 import org.example.model.InMemoryTaskRepository;
-import org.example.model.Task;
 import org.example.model.TaskRepository;
 import org.example.view.MainFrame;
 
@@ -13,32 +13,13 @@ public class Main {
 
         TaskRepository repository = new InMemoryTaskRepository();
 
-        // 2. Создаем пару задач
-        Task t1 = new Task("Убраться", "Помыть полы");
-        Task t2 = new Task("Учеба", "Выучить SOLID");
-        Task t3 = new Task("111", "Выучить xjnnj");
+        TaskController controller = new TaskController(repository);
 
-        // 3. Сохраняем их (тут им присвоятся ID)
-        repository.save(t1);
-        repository.save(t2);
 
-        // 4. Проверяем, что они сохранились и получили ID
-        System.out.println("Все задачи:");
-        for (Task t : repository.findAll()) {
-            System.out.println(t);
-        }
 
-        // 5. Пробуем удалить первую задачу
-        repository.deleteById(1);
-        repository.save(t3);
-
-        System.out.println("После удаления ID=1:");
-        for (Task t : repository.findAll()) {
-            System.out.println(t);
-        }
 
         SwingUtilities.invokeLater(() ->{
-            MainFrame frame = new MainFrame();
+            MainFrame frame = new MainFrame(controller);
             frame.setVisible(true);
         });
     }
--- src/main/java/org/example/view/MainFrame.java
+++ src/main/java/org/example/view/MainFrame.java
@@ -1,7 +1,20 @@
 package org.example.view;
 
+import org.example.controller.TaskController;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+
+    private TaskController controller;
+
+    public MainFrame(TaskController controller){
+        this.controller =  controller;
+        setTitle("TODO");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+
 
 }
